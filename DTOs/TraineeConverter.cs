af48396e83e76f136483a240b779914ac8870b04
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-05 17:47:49 +0530

    Initialize repository

---
+++ DTOs/TraineeConverter.cs
@@ -0,0 +1,57 @@
+using TraineeManagementApi.Models;
+
+namespace TraineeManagementApi.DTOs;
+
+public static class TraineeConverter
+{
+    public static TraineeResponse ToTraineeResponse(Trainee trainee)
+    {
+        return new TraineeResponse
+        {
+            Id = trainee.Id,
+            FirstName = trainee.FirstName,
+            LastName = trainee.LastName,
+            Email = trainee.Email,
+            TechStack = trainee.TechStack,
+            Status = trainee.Status,
+            CreatedDate = trainee.CreatedDate,
+            UpdatedDate = trainee.UpdatedDate
+        };
+    }
+
+    public static Trainee ToTrainee(CreateTraineeRequest request)
+    {
+        return new Trainee
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            TechStack = request.TechStack,
+            Status = request.Status.ToString(),
+        };
+    }
+
+
+    public static Trainee ToTrainee(UpdateTraineeRequest request)
+    {
+        return new Trainee
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            TechStack = request.TechStack,
+            Status = request.Status,
+        };
+    }
+
+    public static List<TraineeResponse> ToTraineeResponseList(List<Trainee> trainees)
+    {
+        List<TraineeResponse> traineeResponses = new();
+
+        foreach (var trainee in trainees) traineeResponses.Add(ToTraineeResponse(trainee));
+
+        return traineeResponses;
+
+    }
+}
+
---
+++ Services/TraineeService.cs
@@ -0,0 +1,83 @@
+using TraineeManagementApi.DTOs;
+using TraineeManagementApi.Models;
+
+namespace TraineeManagementApi.Services;
+
+public class TraineeService : ITraineeService
+{
+    private static readonly List<Trainee> trainees = new();
+
+    // private readonly AppDbContext _context;
+
+    // public TraineeService(AppDbContext context)
+    // {
+    //     _context = context;
+    // }
+
+    public List<TraineeResponse> GetAllTrainees()
+    {
+        return TraineeConverter.ToTraineeResponseList(trainees);
+    }
+
+    public TraineeResponse? GetTraineeResponseById(string traineeId)
+    {
+        foreach(var trainee in trainees)
+            if (trainee.Id == traineeId)
+                return TraineeConverter.ToTraineeResponse(trainee);
+
+        return null;
+        // return _context.Trainees.Find(traineeId);
+    }
+
+    public Trainee? GetTraineeById(string traineeId)
+    {
+        foreach(var trainee in trainees) if (trainee.Id == traineeId) return trainee;
+
+        return null;
+        // return _context.Trainees.Find(traineeId);
+    }
+
+    public TraineeResponse CreateTrainee(CreateTraineeRequest createTraineeRequest)
+    {
+        Trainee trainee = TraineeConverter.ToTrainee(createTraineeRequest);
+        trainee.Id = Guid.NewGuid().ToString();
+        trainee.CreatedDate = DateTime.UtcNow;
+        trainee.UpdatedDate = DateTime.UtcNow;
+
+        trainees.Add(trainee);
+
+        // EF Core pattern:
+        // _context.Trainees.Add(trainee);
+        // _context.SaveChanges();
+
+        return TraineeConverter.ToTraineeResponse(trainee);
+    }
+
+    public TraineeResponse? UpdateTrainee(string id, UpdateTraineeRequest request)
+    {
+        Trainee updateTrainee = TraineeConverter.ToTrainee(request);
+
+        foreach (var trainee in trainees)
+        {
+            if (trainee.Id.Equals(id))
+            {
+                trainee.FirstName = updateTrainee.FirstName;
+                trainee.LastName = updateTrainee.LastName;
+                trainee.Status = updateTrainee.Status;
+                trainee.TechStack = updateTrainee.TechStack;
+                trainee.Email = updateTrainee.Email;
+                trainee.UpdatedDate = DateTime.UtcNow;
+            }
+
+            return TraineeConverter.ToTraineeResponse(trainee);
+        }
+
+        return null;
+    }
+
+    public bool DeleteTraineeById(string traineeId)
+    {
+        return trainees.Remove(GetTraineeById(traineeId));
+    }
+}
+
