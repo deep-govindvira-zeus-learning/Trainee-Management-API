cc789d26db371dfec7cdbea7806002906173618b
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-09 13:45:41 +0530

    feat: Handled db connection exception

--- obj/Debug/net10.0/TraineeManagementApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TraineeManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraineeManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5e149c280173529ec6588ed4c1ae41f820916d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3841ecb3361e1d744cf0826326ff32d2a7a608")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraineeManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraineeManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
