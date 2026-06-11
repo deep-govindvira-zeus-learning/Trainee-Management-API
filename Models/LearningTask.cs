a8f7abe7220803a55e8deeebf6434ae60aecc5d8
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-11 18:21:52 +0530

    feat: Created LearningTask Model class

---
+++ Models/LearningTask.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class LearningTask
+{
+    [Key]
+    public string Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
+    public string ExpectedTechStack { get; set; }
+
+    [Required]
+    public DateTime DueDate { get; set; }
+
+    [Required]
+    public string Status { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedDate { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedDate { get; set; }
+
+}
