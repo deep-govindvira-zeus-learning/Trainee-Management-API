e28e9a9559f0eddc832d86c91a533378a0301364
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-11 17:53:18 +0530

    feat: Added DELETE mentor api

--- DTOs/MentorConverter.cs
+++ DTOs/MentorConverter.cs
@@ -19,6 +19,19 @@ public static class MentorConverter
         };
     }
 
+    public static Mentor ToMentor(CreateMentorRequest request)
+    {
+        return new Mentor
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Expertise = request.Expertise,
+            Status = request.Status
+        };
+    }
+
     public static List<MentorResponse> ToMentorResponseList(List<Mentor> mentors)
     {
         List<MentorResponse> responses = new();
