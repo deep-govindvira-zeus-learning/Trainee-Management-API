8ec40d47d2c3e48d72d4dee136ce1408023b2d2c
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:07:33 +0530

    refactor: Improved code readability

--- DTOs/LoginRequest.cs
+++ DTOs/LoginRequest.cs
@@ -4,9 +4,9 @@ namespace TraineeManagementApi.DTOs;
 
 public class LoginRequest
 {
-    [Required]
-    public string Username { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; set; }
 
-    [Required]
-    public string Password { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
 }
