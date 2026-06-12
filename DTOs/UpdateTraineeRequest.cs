ba753cc612cac22c67a0dbb727b59bfd7b6a64ef
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:59:17 +0530

    fix: Fix Grammer

--- DTOs/UpdateTraineeRequest.cs
+++ DTOs/UpdateTraineeRequest.cs
@@ -4,22 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 public class UpdateTraineeRequest
 {
-    [Required(ErrorMessage = "First name is required")]
-    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name")]
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name.")]
     public string FirstName { get; set; }
     
-    [Required(ErrorMessage = "Last name is required")]
-    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name")]
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name.")]
     public string LastName { get; set; }
 
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Valid email is required")]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Valid email is required.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "TechStack is required")]
+    [Required(ErrorMessage = "TechStack is required.")]
     public string TechStack { get; set; }
 
-    [Required(ErrorMessage = "Status is required")]
-    [AllowedValues("Active", "Inactive", "Completed", ErrorMessage = "Status must be Active, Inactive, or Completed")]
+    [Required(ErrorMessage = "Status is required.")]
+    [AllowedValues("Active", "Inactive", "Completed", ErrorMessage = "Status must be Active, Inactive, or Completed.")]
     public string Status { get; set; }
 }
