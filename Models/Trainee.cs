7aef3a045d8fcc9282231dc3844161c17d7e7887
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:56:24 +0530

    refactor: Improved code readability

--- Models/Trainee.cs
+++ Models/Trainee.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TraineeManagementApi.Models;
 
 public class Trainee
 {
+    [Key]
     public string Id { get; set; }
+
+    [Required(ErrorMessage = "First name is required")]
+    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name")]
     public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Last name is required")]
+    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name")]
     public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Valid email is required")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "TechStack is required")]
     public string TechStack { get; set; }
+
+    [Required(ErrorMessage = "Status is required")]
+    [AllowedValues("Active", "Inactive", "Completed", ErrorMessage = "Status must be Active, Inactive, or Completed")]
     public string Status { get; set; }
     
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -16,6 +32,5 @@ public class Trainee
 
     [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTime UpdatedDate { get; set; }
-
 }
 
