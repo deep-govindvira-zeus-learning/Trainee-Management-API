10ebf6f685cdf830ef85c7a6e513810ab3f4ae61
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:12:34 +0530

    refactor: Improved code readability

--- DTOs/UpdateMentorRequest.cs
+++ DTOs/UpdateMentorRequest.cs
@@ -4,22 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 public class UpdateMentorRequest
 {
-    [Required(ErrorMessage = "First name is required")]
-    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name")]
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name.")]
     public string FirstName { get; set; }
     
-    [Required(ErrorMessage = "Last name is required")]
-    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name")]
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name.")]
     public string LastName { get; set; }
 
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Valid email is required")]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Valid email is required.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Expertise is required")]
+    [Required(ErrorMessage = "Expertise is required.")]
     public string Expertise { get; set; }
 
-    [Required(ErrorMessage = "Status is required")]
-    [AllowedValues("Active", "Inactive", ErrorMessage = "Status must be Active or Inactive")]
+    [Required(ErrorMessage = "Status is required.")]
+    [AllowedValues("Active", "Inactive", ErrorMessage = "Status must be Active or Inactive.")]
     public string Status { get; set; }
 }
