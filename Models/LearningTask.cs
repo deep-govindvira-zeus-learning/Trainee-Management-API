472a6205a775a42296e97737ef54b6034ad7cbac
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:50:15 +0530

    refactor: Improved code redability

--- Models/LearningTask.cs
+++ Models/LearningTask.cs
@@ -6,20 +6,22 @@ public class LearningTask
     [Key]
     public string Id { get; set; }
 
-    [Required]
-    [MaxLength(50)]
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in Title.")]
     public string Title { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Description is required.")]
     public string Description { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "ExpectedTechStack is required.")]
     public string ExpectedTechStack { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "DueDate is required.")]
+    [DataType(DataType.Date, ErrorMessage = "Invalid date format.")]
     public DateOnly DueDate { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Status is required.")]
+    [AllowedValues("Draft", "Published", "Closed", ErrorMessage = "Status must be Draft, Published, or Closed.")]
     public string Status { get; set; }
 
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
