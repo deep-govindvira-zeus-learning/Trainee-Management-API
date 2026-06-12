5dd06adee0680b5b6feef9f565044efbdb8fbeb1
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-12 16:50:15 +0530

    refactor: Improved code redability

--- Models/Mentor.cs
+++ Models/Mentor.cs
@@ -6,23 +6,23 @@ public class Mentor
     [Key]
     public string Id { get; set; }
 
-    [Required]
-    [MaxLength(50)]
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "Maximum 50 characters are allowed in First name.")]
     public string FirstName { get; set; }
-
-    [Required]
-    [MaxLength(50)]
+    
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage ="Maximum 50 characters are allowed in Last name.")]
     public string LastName { get; set; }
 
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Valid email is required.")]
     public string Email { get; set; }
 
-    [Required]
-    [MaxLength(100)]
+    [Required(ErrorMessage = "Expertise is required.")]
     public string Expertise { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Status is required.")]
+    [AllowedValues("Active", "Inactive", ErrorMessage = "Status must be Active or Inactive.")]
     public string Status { get; set; }
 
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
