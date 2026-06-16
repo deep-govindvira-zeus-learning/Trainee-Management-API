b7655de9bc202ef41dba5cf31c72598bd382ef0d
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-16 11:37:41 +0530

    refactor: Improved code readability

--- DTOs/ReviewConverter.cs
+++ DTOs/ReviewConverter.cs
@@ -34,16 +34,6 @@ public static class ReviewConverter
 
     public static List<ReviewResponse> ToReviewResponseList(List<Review> reviews)
     {
-
-        return reviews.Select(review => new ReviewResponse
-        {
-            Id = review.Id,
-            SubmissionId = review.SubmissionId,
-            MentorId = review.MentorId,
-            Feedback = review.Feedback,
-            Score = review.Score,
-            ReviewStatus = review.ReviewStatus,
-            ReviewedDate = review.ReviewedDate
-        }).ToList();
+        return reviews.Select(ToReviewResponse).ToList();
     }
 }
