d9c36d17a4f7039f4595c1b2d44ea9aca4ba3279
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-17 11:54:13 +0530

    fix: Removed warnings

--- DTOs/LoginRequest.cs
+++ DTOs/LoginRequest.cs
@@ -5,8 +5,8 @@ namespace TraineeManagementApi.DTOs;
 public class LoginRequest
 {
     [Required(ErrorMessage = "Username is required.")]
-    public string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required.")]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 }
