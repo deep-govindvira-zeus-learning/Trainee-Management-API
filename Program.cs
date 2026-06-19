95bf50f648971315871a5be2a3e80583adf499a7
Deep Govindvira <deep.govindvira@zeuslearning.mumbai>
2026-06-19 11:52:41 +0530

    refactor: Improved code readability

--- Program.cs
+++ Program.cs
@@ -115,27 +115,27 @@ using (var scope = app.Services.CreateScope())
             new Trainee
             {
                 Id = "208b07fc-0511-4387-8f57-80f332381a4a",
-                FirstName = "Deep",
-                LastName = "Govindvira",
-                Email = "deep.govindvira@zeuslearning.com",
+                FirstName = "Trainee-1",
+                LastName = "Trainee-1-Surname",
+                Email = "trainne.1@example.com",
                 TechStack = "C#, .NET",
                 Status = "Active"
             },
             new Trainee
             {
                 Id = "3631c4b3-c824-4374-a1ef-204237fccfac",
-                FirstName = "Yash",
-                LastName = "Gokulgandhi",
-                Email = "yash.gokulgandhi@zeuslearning.com",
+                FirstName = "Trainee-2",
+                LastName = "Trainee-2-Surname",
+                Email = "trainne.2@example.com",
                 TechStack = "Java, Spring Boot",
                 Status = "InActive",
             },
             new Trainee
             {
                 Id = "522d1097-e6e4-4b91-807f-ca8358fd618e",
-                FirstName = "Divyang",
-                LastName = "Dhameliya",
-                Email = "divyang.dhameliya@zeuslearning.com",
+                FirstName = "Trainee-3",
+                LastName = "Trainee-3-Surname",
+                Email = "trainne.3@example.com",
                 TechStack = "React, Node",
                 Status = "Completed",
             }
@@ -149,18 +149,18 @@ using (var scope = app.Services.CreateScope())
             new Mentor
             {
                 Id = "2fdc755a-5118-4ed8-9e21-300133d7c088",
-                FirstName = "Kedar",
-                LastName = "Kale",
-                Email = "kedar.kale@zeuslearning.com",
+                FirstName = "Mentor-1",
+                LastName = "Mentor-1-Surname",
+                Email = "mentor.1@example.com",
                 Expertise = "C#, .NET, MERN, Angular, AWS",
                 Status = "Active",
             },
             new Mentor
             {
                 Id = "8f9241fe-9464-4d57-97d8-6964c67ff98d",
-                FirstName = "Tanuj",
-                LastName = "Kude",
-                Email = "tanuj.kude@zeuslearning.com",
+                FirstName = "Mentor-2",
+                LastName = "Mentor-2-Surname",
+                Email = "mentor.2@example.com",
                 Expertise = "C#, .NET",
                 Status = "Inactive",
             }
@@ -188,7 +188,7 @@ using (var scope = app.Services.CreateScope())
         {
             Id = Guid.NewGuid().ToString(),
             Username = "mentor",
-            Email = "mentor@zeuslearning.com",
+            Email = "mentor@test.com",
             PasswordHash = BCrypt.Net.BCrypt.HashPassword("Mentor@123"),
             Role = UserRole.Mentor,
         });
@@ -200,8 +200,8 @@ using (var scope = app.Services.CreateScope())
         context.LearningTasks.AddRange(new LearningTask
         {
             Id = "036d2863-ff85-4861-8843-aa9f150bedec",
-            Title = "Task Tracker",
-            Description = "This task is about HTML, CSS, Javascript.",
+            Title = "Task-1",
+            Description = "This is description of task-1.",
             ExpectedTechStack = "HTML, CSS, Javascript",
             DueDate = new DateOnly(2026, 6, 7),
             Status = "Closed"
@@ -209,8 +209,8 @@ using (var scope = app.Services.CreateScope())
         new LearningTask
         {
             Id = "0906992e-ded5-4f40-9cbf-b05767b52ba2",
-            Title = "Trainee Management Api - 1",
-            Description = "This task is about learning C#, .NET, MVC.",
+            Title = "Task-2",
+            Description = "This is description of task-2.",
             ExpectedTechStack = "C#, .NET",
             DueDate = new DateOnly(2026, 6, 15),
             Status = "Published"
@@ -228,7 +228,7 @@ using (var scope = app.Services.CreateScope())
             LearningTaskId = "036d2863-ff85-4861-8843-aa9f150bedec",
             AssignedDate = new DateOnly(2026, 7, 6),
             DueDate = new DateOnly(2026, 8, 9),
-            Remarks = "This is remark.",
+            Remarks = "This is remark of this assignment.",
             Status = "Assigned"
         });
         context.SaveChanges();
@@ -241,7 +241,7 @@ using (var scope = app.Services.CreateScope())
             Id = "48a044de-9e2e-4b66-aef5-2b0287a9d0a7",
             AssignmentId = "036d2863-ff85-4861-8843-aa9f150bedec",
             SubmissionUrl = "https://github.com/deep-govindvira-zeus-learning",
-            Notes = "Well done.",
+            Notes = "This is note of this submission.",
             Status = "Submitted",
             SubmittedDate = new DateOnly(2026, 9, 9)
         });
@@ -255,7 +255,7 @@ using (var scope = app.Services.CreateScope())
             Id = "48a0lde-9e2e-90sd-aef5-2b0287a9d0a7",
             SubmissionId = "48a044de-9e2e-4b66-aef5-2b0287a9d0a7",
             MentorId = "2fdc755a-5118-4ed8-9e21-300133d7c088",
-            Feedback = "Greate work.",
+            Feedback = "This is feedback of this review.",
             Score = 7,
             ReviewStatus = "Accepted",
             ReviewedDate = DateOnly.FromDateTime(DateTime.UtcNow)
